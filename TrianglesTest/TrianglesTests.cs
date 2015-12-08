087140c0d76e818b9601e5aab3950822452ca79f
Jen Kli <shikodingo@hotmail.com>
2015-12-08 11:23:17 -0500

    TestFindHypotenuseInvalid test failed

--- TrianglesTest/TrianglesTests.cs
+++ TrianglesTest/TrianglesTests.cs
@@ -24,5 +24,21 @@ namespace TrianglesTest
 
             Assert.AreEqual(expectedH, hypotenuse);
         }
+        [TestMethod]
+        public void TestFindHypotenuseInvalid()
+        {
+            // Variables
+            double side1 = -5.0;
+            double side2 = -6.0;
+            double hypotenuse = 0.0;
+            double expectedH = -7.81;
+
+            hypotenuse = Triangles.Triangles.FindHypotenuse(side1, side2);
+
+            string convert = hypotenuse.ToString("N2");
+            hypotenuse = double.Parse(convert);
+
+            Assert.AreEqual(expectedH, hypotenuse);
+        }
     }
 }
