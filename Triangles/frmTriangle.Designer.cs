31d415b691e51cee1b942db4fdcae623b11fa68f
Jen Kli <shikodingo@hotmail.com>
2015-12-08 10:57:12 -0500

    Ran TestFindHypotenuseValid() and hit build error

--- Triangles/frmTriangle.Designer.cs
+++ Triangles/frmTriangle.Designer.cs
@@ -28,12 +28,253 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblStatus1 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAngle = new System.Windows.Forms.Button();
+            this.btnArea = new System.Windows.Forms.Button();
+            this.btnHypotenuse = new System.Windows.Forms.Button();
+            this.lblAngle3 = new System.Windows.Forms.Label();
+            this.txtAngle3 = new System.Windows.Forms.TextBox();
+            this.lblAngle2 = new System.Windows.Forms.Label();
+            this.lblAngle1 = new System.Windows.Forms.Label();
+            this.lblArea = new System.Windows.Forms.Label();
+            this.lblHyp = new System.Windows.Forms.Label();
+            this.lblSide2 = new System.Windows.Forms.Label();
+            this.lblSide1 = new System.Windows.Forms.Label();
+            this.txtAngle2 = new System.Windows.Forms.TextBox();
+            this.txtAngle1 = new System.Windows.Forms.TextBox();
+            this.txtArea = new System.Windows.Forms.TextBox();
+            this.txtHypotenuse = new System.Windows.Forms.TextBox();
+            this.txtSide2 = new System.Windows.Forms.TextBox();
+            this.txtSide1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblStatus1
+            // 
+            this.lblStatus1.AutoSize = true;
+            this.lblStatus1.Location = new System.Drawing.Point(75, 192);
+            this.lblStatus1.Name = "lblStatus1";
+            this.lblStatus1.Size = new System.Drawing.Size(142, 13);
+            this.lblStatus1.TabIndex = 39;
+            this.lblStatus1.Text = "Only integers are acceptable";
+            this.lblStatus1.Visible = false;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(337, 192);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(142, 13);
+            this.lblStatus.TabIndex = 38;
+            this.lblStatus.Text = "Only integers are acceptable";
+            this.lblStatus.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(293, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(9, 234);
+            this.label1.TabIndex = 37;
+            this.label1.Text = "|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n";
+            // 
+            // btnAngle
+            // 
+            this.btnAngle.Location = new System.Drawing.Point(408, 156);
+            this.btnAngle.Name = "btnAngle";
+            this.btnAngle.Size = new System.Drawing.Size(97, 23);
+            this.btnAngle.TabIndex = 36;
+            this.btnAngle.Text = "Get Angle";
+            this.btnAngle.UseVisualStyleBackColor = true;
+            // 
+            // btnArea
+            // 
+            this.btnArea.Location = new System.Drawing.Point(163, 156);
+            this.btnArea.Name = "btnArea";
+            this.btnArea.Size = new System.Drawing.Size(97, 23);
+            this.btnArea.TabIndex = 35;
+            this.btnArea.Text = "Get Area";
+            this.btnArea.UseVisualStyleBackColor = true;
+            // 
+            // btnHypotenuse
+            // 
+            this.btnHypotenuse.Location = new System.Drawing.Point(34, 156);
+            this.btnHypotenuse.Name = "btnHypotenuse";
+            this.btnHypotenuse.Size = new System.Drawing.Size(97, 23);
+            this.btnHypotenuse.TabIndex = 34;
+            this.btnHypotenuse.Text = "Get Hypotenuse";
+            this.btnHypotenuse.UseVisualStyleBackColor = true;
+            // 
+            // lblAngle3
+            // 
+            this.lblAngle3.AutoSize = true;
+            this.lblAngle3.Location = new System.Drawing.Point(405, 220);
+            this.lblAngle3.Name = "lblAngle3";
+            this.lblAngle3.Size = new System.Drawing.Size(43, 13);
+            this.lblAngle3.TabIndex = 33;
+            this.lblAngle3.Text = "Angle 3";
+            // 
+            // txtAngle3
+            // 
+            this.txtAngle3.Location = new System.Drawing.Point(405, 246);
+            this.txtAngle3.Name = "txtAngle3";
+            this.txtAngle3.ReadOnly = true;
+            this.txtAngle3.Size = new System.Drawing.Size(100, 20);
+            this.txtAngle3.TabIndex = 32;
+            // 
+            // lblAngle2
+            // 
+            this.lblAngle2.AutoSize = true;
+            this.lblAngle2.Location = new System.Drawing.Point(462, 80);
+            this.lblAngle2.Name = "lblAngle2";
+            this.lblAngle2.Size = new System.Drawing.Size(43, 13);
+            this.lblAngle2.TabIndex = 31;
+            this.lblAngle2.Text = "Angle 2";
+            // 
+            // lblAngle1
+            // 
+            this.lblAngle1.AutoSize = true;
+            this.lblAngle1.Location = new System.Drawing.Point(330, 80);
+            this.lblAngle1.Name = "lblAngle1";
+            this.lblAngle1.Size = new System.Drawing.Size(43, 13);
+            this.lblAngle1.TabIndex = 30;
+            this.lblAngle1.Text = "Angle 1";
+            // 
+            // lblArea
+            // 
+            this.lblArea.AutoSize = true;
+            this.lblArea.Location = new System.Drawing.Point(170, 220);
+            this.lblArea.Name = "lblArea";
+            this.lblArea.Size = new System.Drawing.Size(29, 13);
+            this.lblArea.TabIndex = 29;
+            this.lblArea.Text = "Area";
+            // 
+            // lblHyp
+            // 
+            this.lblHyp.AutoSize = true;
+            this.lblHyp.Location = new System.Drawing.Point(31, 220);
+            this.lblHyp.Name = "lblHyp";
+            this.lblHyp.Size = new System.Drawing.Size(64, 13);
+            this.lblHyp.TabIndex = 28;
+            this.lblHyp.Text = "Hypotenuse";
+            // 
+            // lblSide2
+            // 
+            this.lblSide2.AutoSize = true;
+            this.lblSide2.Location = new System.Drawing.Point(170, 80);
+            this.lblSide2.Name = "lblSide2";
+            this.lblSide2.Size = new System.Drawing.Size(37, 13);
+            this.lblSide2.TabIndex = 27;
+            this.lblSide2.Text = "Side 2";
+            // 
+            // lblSide1
+            // 
+            this.lblSide1.AutoSize = true;
+            this.lblSide1.Location = new System.Drawing.Point(31, 80);
+            this.lblSide1.Name = "lblSide1";
+            this.lblSide1.Size = new System.Drawing.Size(37, 13);
+            this.lblSide1.TabIndex = 26;
+            this.lblSide1.Text = "Side 1";
+            // 
+            // txtAngle2
+            // 
+            this.txtAngle2.Location = new System.Drawing.Point(465, 99);
+            this.txtAngle2.Name = "txtAngle2";
+            this.txtAngle2.Size = new System.Drawing.Size(100, 20);
+            this.txtAngle2.TabIndex = 25;
+            // 
+            // txtAngle1
+            // 
+            this.txtAngle1.Location = new System.Drawing.Point(333, 99);
+            this.txtAngle1.Name = "txtAngle1";
+            this.txtAngle1.Size = new System.Drawing.Size(100, 20);
+            this.txtAngle1.TabIndex = 24;
+            // 
+            // txtArea
+            // 
+            this.txtArea.Location = new System.Drawing.Point(163, 246);
+            this.txtArea.Name = "txtArea";
+            this.txtArea.ReadOnly = true;
+            this.txtArea.Size = new System.Drawing.Size(100, 20);
+            this.txtArea.TabIndex = 23;
+            // 
+            // txtHypotenuse
+            // 
+            this.txtHypotenuse.Location = new System.Drawing.Point(30, 246);
+            this.txtHypotenuse.Name = "txtHypotenuse";
+            this.txtHypotenuse.ReadOnly = true;
+            this.txtHypotenuse.Size = new System.Drawing.Size(100, 20);
+            this.txtHypotenuse.TabIndex = 22;
+            // 
+            // txtSide2
+            // 
+            this.txtSide2.Location = new System.Drawing.Point(163, 99);
+            this.txtSide2.Name = "txtSide2";
+            this.txtSide2.Size = new System.Drawing.Size(100, 20);
+            this.txtSide2.TabIndex = 21;
+            // 
+            // txtSide1
+            // 
+            this.txtSide1.Location = new System.Drawing.Point(31, 99);
+            this.txtSide1.Name = "txtSide1";
+            this.txtSide1.Size = new System.Drawing.Size(100, 20);
+            this.txtSide1.TabIndex = 20;
+            // 
+            // frmTriangle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(595, 352);
+            this.Controls.Add(this.lblStatus1);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAngle);
+            this.Controls.Add(this.btnArea);
+            this.Controls.Add(this.btnHypotenuse);
+            this.Controls.Add(this.lblAngle3);
+            this.Controls.Add(this.txtAngle3);
+            this.Controls.Add(this.lblAngle2);
+            this.Controls.Add(this.lblAngle1);
+            this.Controls.Add(this.lblArea);
+            this.Controls.Add(this.lblHyp);
+            this.Controls.Add(this.lblSide2);
+            this.Controls.Add(this.lblSide1);
+            this.Controls.Add(this.txtAngle2);
+            this.Controls.Add(this.txtAngle1);
+            this.Controls.Add(this.txtArea);
+            this.Controls.Add(this.txtHypotenuse);
+            this.Controls.Add(this.txtSide2);
+            this.Controls.Add(this.txtSide1);
+            this.Name = "frmTriangle";
+            this.Text = "Triangle";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblStatus1;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAngle;
+        private System.Windows.Forms.Button btnArea;
+        private System.Windows.Forms.Button btnHypotenuse;
+        private System.Windows.Forms.Label lblAngle3;
+        private System.Windows.Forms.TextBox txtAngle3;
+        private System.Windows.Forms.Label lblAngle2;
+        private System.Windows.Forms.Label lblAngle1;
+        private System.Windows.Forms.Label lblArea;
+        private System.Windows.Forms.Label lblHyp;
+        private System.Windows.Forms.Label lblSide2;
+        private System.Windows.Forms.Label lblSide1;
+        private System.Windows.Forms.TextBox txtAngle2;
+        private System.Windows.Forms.TextBox txtAngle1;
+        private System.Windows.Forms.TextBox txtArea;
+        private System.Windows.Forms.TextBox txtHypotenuse;
+        private System.Windows.Forms.TextBox txtSide2;
+        private System.Windows.Forms.TextBox txtSide1;
     }
 }
 
--- TrianglesTest/TrianglesTests.cs
+++ TrianglesTest/TrianglesTests.cs
@@ -7,8 +7,16 @@ namespace TrianglesTest
     public class TrianglesTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestFindHypotenuseValid()
         {
+            double side1 = 5;
+            double side2 = 6;
+            double hypotenuse = 0;
+            double expectedH = 7.81;
+
+           hypotenuse = FindHypotenuse(side1, side2);
+
+            Assert.AreEqual(expectedH, hypotenuse);
         }
     }
 }
